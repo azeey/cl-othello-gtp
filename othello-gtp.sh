9e4799a09aae8b8f771177663f9220cbe73fdcf9
Addisu Taddese <addisu.taddese@ciholas.com>
2012-12-11 17:56:34 -0600

    Added clisp as an alternative to sbcl in othello-gtp.sh

--- othello-gtp.sh
+++ othello-gtp.sh
@@ -2,8 +2,12 @@
 
 # Script to launch Othello as an engine for Quarry
 #
-# usage : othello-gtp.sh
+# usage : othello-gtp.sh [strategy index]
 
 cd $(dirname $0)
 
 sbcl --script "othello-gtp.lisp" $@
+
+# Use this if using clisp
+#clisp -C "othello-gtp.lisp" $@
+
